e30bb44cf1744efcdf2060da3fda18e906fdfab7
Robin Miller & Ben Birdsall <student@epicodus.com>
2014-03-07 17:09:35 -0800

    corrected syntax

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,6 +1,6 @@
 def numbers_to_words(number, index = 0)
-  ones_words = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 70 => "seventeen", 18 => "eighteen", 19 => "nineteen", 0 => ""}
-  decades = {0 => "", 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety"}
+  ones_words = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 70 => "seventeen", 18 => "eighteen", 19 => "nineteen", 0 => "\b"}
+  decades = {0 => "\b", 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety"}
   magnitudes = ["", "thousand", "million", "billion", "trillion"]
   result = []
   if number > 999
@@ -12,5 +12,5 @@ def numbers_to_words(number, index = 0)
   elsif number > 0
     result << ones_words[number] << magnitudes[index]
   end
-  result.join(" ").squeeze(" ").strip
+  result.join(" ").squeeze(' ').strip
 end
